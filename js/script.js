4c78da9061821e730b461242fb34f3aa9923497e
SHIVAM <shivam@SHIVAMs-MacBook.local>
2024-05-24 18:33:14 +0530

    css updated & js/script.js file exclusion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// this is not included as GIT Pages is not supporting this structure.
 let typeEffect = new Typed('#element', {
     strings: ["Java Developer","Noob Gamer", "Coder."],
     loop: true,
